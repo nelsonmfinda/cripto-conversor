ac904ae1f5d69051667b2d6467825da00a22f589
Nelson Mfinda <nelsonmfinda@hotmail.com>
2019-11-25 06:43:17 +0100

    Create main app component

---
+++ src/components/Conversor.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1>Criptocurrency Conversor</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Conversor",
+  props: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
