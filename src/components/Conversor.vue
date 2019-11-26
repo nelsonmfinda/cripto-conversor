efec44996f76bfd8d87c74695c32244f9ca819d1
Nelson Mfinda <nelsonmfinda@hotmail.com>
2019-11-26 05:38:18 +0100

    Create form and methods form conversion

--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -1,13 +1,99 @@
 <template>
   <div class="container">
-    <h1>Criptocurrency Conversor</h1>
+    <h1 class="h1">Criptocurrency Conversor</h1>
+    <div class="columns">
+      <div class="column is-three-fifths">
+        <form>
+          <div class="field">
+            <label class="label">From</label>
+            <div class="control">
+              <input
+                v-model="from"
+                class="input"
+                type="text"
+                placeholder="Text input"
+              />
+            </div>
+            <p class="help">This is a help text</p>
+          </div>
+          <div class="field">
+            <label class="label">To</label>
+            <div class="control">
+              <input
+                v-model="to"
+                class="input"
+                type="text"
+                placeholder="Text input"
+              />
+            </div>
+            <p class="help">This is a help text</p>
+          </div>
+          <div class="field">
+            <label class="label">Amount</label>
+            <div class="control">
+              <input
+                v-model="amount"
+                class="input"
+                type="text"
+                placeholder="Text input"
+              />
+            </div>
+            <p class="help">This is a help text</p>
+          </div>
+          <div class="control">
+            <button
+              @click.prevent="convertCurrency()"
+              class="button is-primary"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Conversor",
-  props: {}
+  props: {},
+  data: function() {
+    return {
+      from: 0,
+      to: 0,
+      result: "",
+      amount: ""
+    };
+  },
+  methods: {
+    async convertCurrency() {
+      try {
+        this.axios
+          .get(
+            `/convert?q=${this.from}_${this.to}&compact=ultra&apiKey=${process.env.API_KEY}`
+          )
+          .then(response => {
+            // console.log(response);
+            this.result = response;
+          });
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async getCurrencies() {
+      try {
+        this.axios
+          .get(`/currencies?apiKey=${process.env.API_KEY}`)
+          .then(response => {
+            // console.log(response);
+            this.result = response;
+          });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
 };
 </script>
 
