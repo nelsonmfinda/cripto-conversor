310267426509ff9b10cfb5309fc898df459e9172
Nelson Mfinda <nelsonmfinda@hotmail.com>
2019-11-26 05:38:48 +0100

    Configure axios and vue-axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
+
+axios.defaults.baseURL = process.env.BASE_URL
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
