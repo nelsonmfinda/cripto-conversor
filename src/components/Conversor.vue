c0eed1f10ba246341c0571d8a51a7a41c82ad0f5
Nelson Mfinda <nelsonmfinda@hotmail.com>
2019-11-27 06:14:42 +0100

    Implement feature currency conversion

--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -1,101 +1,102 @@
 <template>
   <div class="container">
-    <h1 class="h1">Criptocurrency Conversor</h1>
-    <div class="columns">
-      <div class="column is-three-fifths">
-        <form>
-          <div class="field">
-            <label class="label">From</label>
-            <div class="control">
-              <input
-                v-model="from"
-                class="input"
-                type="text"
-                placeholder="Text input"
-              />
-            </div>
-            <p class="help">This is a help text</p>
-          </div>
-          <div class="field">
-            <label class="label">To</label>
-            <div class="control">
-              <input
-                v-model="to"
-                class="input"
-                type="text"
-                placeholder="Text input"
-              />
-            </div>
-            <p class="help">This is a help text</p>
-          </div>
-          <div class="field">
-            <label class="label">Amount</label>
-            <div class="control">
-              <input
-                v-model="amount"
-                class="input"
-                type="text"
-                placeholder="Text input"
-              />
-            </div>
-            <p class="help">This is a help text</p>
-          </div>
-          <div class="control">
-            <button
-              @click.prevent="convertCurrency()"
-              class="button is-primary"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
+    <form class="col-md-6 mt-5 offset-3">
+      <div class="form-group">
+        <label class="float-left" for="amount">Amount</label>
+        <input type="text" class="form-control" id="amount" v-model="amount">
       </div>
-    </div>
+      <div class="form-group">
+        <label class="float-left" for="from">From</label>
+        <select v-model="from" class="form-control" id="from">
+          <option v-for="currency in currencies"
+                  :value="currency.id"
+                  :key="currency.id"
+          >
+          {{ currency.currencyName }}
+          </option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label class="float-left" for="to">To</label>
+        <select v-model="to" class="form-control" id="to">
+          <option v-for="currency in currencies"
+                  :value="currency.id"
+                  :key="currency.id"
+          >
+            {{ currency.currencyName }}
+          </option>
+        </select>
+      </div>
+      <div class="form-group">
+        <p class="font-weight-bold p-2 result">{{ calculateConversion }}</p>
+      </div>
+      <button @click.prevent="convertCurrency()" class="btn btn-primary btn-block">Convert</button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Conversor",
+  name: 'Conversor',
   props: {},
-  data: function() {
+  data: function () {
     return {
-      from: 0,
-      to: 0,
-      result: "",
-      amount: ""
-    };
+      from: 'BTC',
+      to: 'USD',
+      currencies: '',
+      amount: 0,
+      result: 0
+    }
+  },
+  computed: {
+    calculateConversion: function () {
+      return (Number(this.amount) * this.result).toFixed(3)
+    }
   },
   methods: {
-    async convertCurrency() {
+    async convertCurrency () {
+      const query = `${this.from}_${this.to}`
       try {
         this.axios
           .get(
-            `/convert?q=${this.from}_${this.to}&compact=ultra&apiKey=${process.env.API_KEY}`
+            `/convert?q=${query}&compact=ultra&apiKey=${process.env.VUE_APP_TOKEN}`
           )
           .then(response => {
-            // console.log(response);
-            this.result = response;
-          });
+            this.result = response.data[query]
+            console.log(response.data[query])
+          })
       } catch (e) {
-        console.log(e);
+        console.log(e)
       }
     },
-    async getCurrencies() {
+    async getCurrencies () {
       try {
         this.axios
-          .get(`/currencies?apiKey=${process.env.API_KEY}`)
+          .get(`/currencies?apiKey=${process.env.VUE_APP_TOKEN}`)
           .then(response => {
-            // console.log(response);
-            this.result = response;
-          });
+            this.currencies = response.data.results
+            console.log(this.currencies)
+          })
       } catch (e) {
-        console.log(e);
+        console.log(e)
       }
     }
+  },
+  created () {
+    this.getCurrencies()
   }
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+  .page-title {
+    margin-top: 20px;
+    text-transform: uppercase;
+    padding: 3em;
+  }
+
+  .result {
+    font-size: 2em;
+  }
+</style>
